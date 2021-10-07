22c9788458fa3c3d8e2b818769255c56d1edc1eb
Smruti <lenkasmruti123@gmail.com>
2021-10-07 20:21:03 +0530

    Added: Uc2_AddNewContact

--- AddressBook/AddressBookMain.java
+++ AddressBook/AddressBookMain.java
@@ -1,26 +1,44 @@
 package AddressBook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
-
 	public static void main(String[] args) {
-		System.out.println("Welcome to AddressBook Program");
-		AddressBook addressbook = new AddressBook();
+		  AddressBook addressbook = new AddressBook();
 
-		addressbook.setFirstname("smruti");
-		addressbook.setLastname("lenka");
-		addressbook.setZip("746046");
-		addressbook.setAddress("barapur");
-		addressbook.setStat("bhadrak");
-		addressbook.setCity("Basudevpur");
-		addressbook.setEmail("slenka132@gmail.com");
-		addressbook.setPhoneNumber("7504614661");
+			addressbook.setFirstname("smruti");
+			addressbook.setLastname("lenka");
+			addressbook.setZip("746046");
+			addressbook.setAddress("barapur");
+			addressbook.setStat("bhadrak");
+			addressbook.setCity("Basudevpur");
+			addressbook.setEmail("slenka132@gmail.com");
+			addressbook.setPhoneNumber("7504614661");
+			
+			AddressBook addressbook1 = new AddressBook();
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter details  new person firstName,lastName,address,city,state,zip,phoneNumber,Email ");
+			addressbook1.setFirstname(sc.nextLine());
+			addressbook1.setLastname(sc.nextLine());
+			addressbook1.setAddress(sc.nextLine());
+			addressbook1.setCity(sc.nextLine());
+			addressbook1.setStat(sc.nextLine());
+			addressbook1.setZip(sc.nextLine());
+			addressbook1.setPhoneNumber(sc.nextLine());
+			addressbook1.setEmail(sc.nextLine());
+			sc.close();
 
-		ContactStore contactStore = new ContactStore();
-		contactStore.add(addressbook);
 
-		UserInterface user = new UserInterface();
-		user.print(contactStore.getContactList());
+			ContactStore contactStore = new ContactStore();
+			contactStore.add(addressbook);
+			contactStore.add(addressbook1);
 
+
+			UserInterface user = new UserInterface();
+			user.print(contactStore.getContactList());
+
+		}
 	}
-}
+
+
 
--- AddressBook/UserInterface.java
+++ AddressBook/UserInterface.java
@@ -3,14 +3,12 @@ package AddressBook;
 import java.util.Set;
 
 public class UserInterface {
-	public void print(Set<AddressBook> contactList) {
-		for (AddressBook adsbook : contactList) {
-			System.out.println(adsbook);
+	void print(Set<AddressBook> contactList) {
+		for (AddressBook s : contactList) {
+			System.out.println(s);
 		}
 	}
+}
 
-
-		
-	}
 
 
