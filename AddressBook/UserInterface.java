fc075bdbe6a1913cf785d0b88a9fd07997904f9f
Smruti234 <89955654+Smruti234@users.noreply.github.com>
2021-10-09 19:29:36 +0530

    Update UserInterface.java

--- AddressBook/UserInterface.java
+++ AddressBook/UserInterface.java
@@ -9,7 +9,8 @@ public class UserInterface {
 			System.out.println(sc);
 		}
 	}
-	static void edit(AddressBook contact) {
+
+	static void remove(AddressBook contact) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Edit the details firstName,lastName,address,city,state,zip,phoneNumber,email ");
 		contact.setFirstname(sc.nextLine());
@@ -20,22 +21,10 @@ public class UserInterface {
 		contact.setZip(sc.nextLine());
 		contact.setPhoneNumber(sc.nextLine());
 		contact.setEmail(sc.nextLine());
+
 	}
-	
-	 static void remove(AddressBook contact) {
-		 Scanner sc = new Scanner(System.in);
-			System.out.println("Edit the details firstName,lastName,address,city,state,zip,phoneNumber,email ");
-			contact.setFirstname(sc.nextLine());
-			contact.setLastname(sc.nextLine());
-			contact.setAddress(sc.nextLine());
-			contact.setCity(sc.nextLine());
-			contact.setStat(sc.nextLine());
-			contact.setZip(sc.nextLine());
-			contact.setPhoneNumber(sc.nextLine());
-			contact.setEmail(sc.nextLine());
-		
+
+	public static void delete(AddressBook addressbook) {
+
 	}
 }
-
-
-
